58b829c58185c77b4a017ea19c2c1caf284264e4
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-09-30 11:09:04 +0530

    First Commit

---
+++ src/main/java/com/jitendra/eduapp/EduAppApplication.java
@@ -0,0 +1,12 @@
+package com.jitendra.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduAppApplication.class, args);
+	}
+}
