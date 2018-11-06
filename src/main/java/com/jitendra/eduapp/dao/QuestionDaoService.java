9da1e19aa00f23a5f9bbceb675248b29e45484fd
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2018-11-06 18:02:49 +0530

    Added mcq

--- src/main/java/com/jitendra/eduapp/dao/QuestionDaoService.java
+++ src/main/java/com/jitendra/eduapp/dao/QuestionDaoService.java
@@ -1,7 +1,11 @@
 package com.jitendra.eduapp.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.jitendra.eduapp.domin.Question;
+import com.jitendra.eduapp.enums.QuestionType;
 import com.jitendra.eduapp.repository.QuestionRepository;
 /**
  * @author jitendra sagoriya
@@ -9,5 +13,9 @@ import com.jitendra.eduapp.repository.QuestionRepository;
  */
 @Service
 public interface QuestionDaoService extends BaseDaoService<QuestionRepository> {
+	
+	public Page<Question> getByChapter(String id,Pageable pageable);
+
+	Page<Question> getByChapter(String id, QuestionType type, Pageable pageable);
 
 }
--- src/main/java/com/jitendra/eduapp/dao/impl/QuestionDaoServiceImpl.java
+++ src/main/java/com/jitendra/eduapp/dao/impl/QuestionDaoServiceImpl.java
@@ -1,14 +1,16 @@
 package com.jitendra.eduapp.dao.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.jitendra.eduapp.dao.QuestionDaoService;
+import com.jitendra.eduapp.domin.Question;
+import com.jitendra.eduapp.enums.QuestionType;
 import com.jitendra.eduapp.repository.QuestionRepository;
-/**
- * @author jitendra sagoriya
- *
- */
+ 
 @Service
 public class QuestionDaoServiceImpl implements QuestionDaoService {
 	
@@ -18,6 +20,20 @@ public class QuestionDaoServiceImpl implements QuestionDaoService {
 	@Override
 	public QuestionRepository getRepository() {
 		return questionRepository;
+	}
+
+	@Override
+	public Page<Question> getByChapter(String id, Pageable pageable) {
+		Example<Question> videoExample = Example.of(new Question(null, null, null,Long.valueOf(id) , null));
+		Page<Question> questions =  questionRepository.findAll(videoExample, pageable);
+		return questions;
+	}
+	
+	@Override
+	public Page<Question> getByChapter(String id, QuestionType type, Pageable pageable) {
+		Example<Question> videoExample = Example.of(new Question(type, null, null,Long.valueOf(id) , null));
+		Page<Question> questions =  questionRepository.findAll(videoExample, pageable);
+		return questions;
 	}
 
 }
